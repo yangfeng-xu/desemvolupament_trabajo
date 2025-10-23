e3a7a8dfbfb7bd815da3d3da24ebba1e8a13263e
yangfeng-xu <yangfeng.xu@estudiantat.upc.edu>
2025-10-23 09:26:08 +0200

    f

---
+++ Assets/Textures/fan_made_megaman_8_bit_spritesheet__old__by_johncartoonist04_de34zsp-fullview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="fan_made_megaman_8_bit_spritesheet__old__by_johncartoonist04_de34zsp-fullview" tilewidth="32" tileheight="32" tilecount="1920" columns="40">
+ <image source="../../../../../Downloads/fan_made_megaman_8_bit_spritesheet__old__by_johncartoonist04_de34zsp-fullview.png" width="1280" height="1536"/>
+</tileset>
