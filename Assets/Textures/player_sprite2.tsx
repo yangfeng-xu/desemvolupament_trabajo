6ecb95314e89a4f1cf400e96b2de63ca383ad017
yangfeng-xu <yangfeng.xu@estudiantat.upc.edu>
2025-10-23 09:43:43 +0200

    hhhh

---
+++ Assets/Textures/player_sprite2.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="player_sprite" tilewidth="32" tileheight="32" tilecount="169" columns="13">
+ <image source="../../../../../Downloads/player_sprite.png" width="446" height="444"/>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="39">
+  <animation>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="42" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="156">
+  <animation>
+   <frame tileid="156" duration="100"/>
+   <frame tileid="157" duration="100"/>
+   <frame tileid="158" duration="100"/>
+   <frame tileid="159" duration="100"/>
+   <frame tileid="160" duration="100"/>
+   <frame tileid="161" duration="100"/>
+   <frame tileid="162" duration="100"/>
+   <frame tileid="163" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
