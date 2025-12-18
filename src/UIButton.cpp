1ead52ce1e18f50572b04e9280fb41d5665b0b41
Bole05 <bole.wu@estudiantat.upc.edu>
2025-12-18 08:51:12 +0100

    Ui code

---
+++ src/UIButton.cpp
@@ -0,0 +1,64 @@
+#include "UIButton.h"
+#include "Render.h"
+#include "Engine.h"
+#include "Audio.h"
+
+UIButton::UIButton(int id, SDL_Rect bounds, const char* text) : UIElement(UIElementType::BUTTON, id)
+{
+	this->bounds = bounds;
+	this->text = text;
+
+	canClick = true;
+	drawBasic = false;
+}
+
+UIButton::~UIButton()
+{
+
+}
+
+bool UIButton::Update(float dt)
+{
+	if (state != UIElementState::DISABLED)
+	{
+		// L16: TODO 3: Update the state of the GUiButton according to the mouse position
+		Vector2D mousePos = Engine::GetInstance().input->GetMousePosition();//retornar la posición del ratón
+		//
+		if (mousePos.getX() > bounds.x && mousePos.getX() < bounds.x + bounds.w &&
+			mousePos.getY() > bounds.y && mousePos.getY() < bounds.y + bounds.h) {
+
+			state = UIElementState::FOCUSED;
+		
+			if (Engine::GetInstance().input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_REPEAT) {//constante mientra que esta pretando el boton
+				state = UIElementState::PRESSED;
+			}
+			if (Engine::GetInstance().input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_UP) {
+				NotifyObserver();
+			}
+		}///asegurar de que el mouse esta dentro del limite
+		else {
+			state = UIElementState::NORMAL;
+		}
+		// L16: TODO 4: Draw the button according the GuiControl State
+		switch (state)
+		{
+		case UIElementState::DISABLED:
+			Engine::GetInstance().render->DrawRectangle(bounds, 200, 200, 200, 255, true, false);
+			break;
+		case UIElementState::NORMAL:
+			Engine::GetInstance().render->DrawRectangle(bounds, 0, 255, 255, 255, true, false);
+			break;
+		case UIElementState::FOCUSED:
+			Engine::GetInstance().render->DrawRectangle(bounds, 0, 0, 20, 255, true, false);
+			break;
+		case UIElementState::PRESSED:
+			Engine::GetInstance().render->DrawRectangle(bounds, 0, 255, 0, 255, true, false);//false no se mueve con la camara
+			break;
+		}
+		// L16: TODO 6: Draw text centered in the button
+		Engine::GetInstance().render->DrawText(text.c_str(), bounds.x, bounds.y, bounds.w, bounds.h, { 255,255,255,255 });
+		
+	}
+
+	return false;
+}
---
+++ src/UIManager.cpp
@@ -0,0 +1,74 @@
+#include "UIManager.h"
+#include "UIButton.h"
+#include "Engine.h"
+#include "Textures.h"
+#include "Audio.h"
+
+UIManager::UIManager() :Module()
+{
+	name = "UIManager";
+}
+
+UIManager::~UIManager() {}
+
+bool UIManager::Start()
+{
+	return true;
+}
+
+std::shared_ptr<UIElement> UIManager::CreateUIElement(UIElementType type, int id, const char* text, SDL_Rect bounds, Module* observer, SDL_Rect sliderBounds)
+{
+	std::shared_ptr<UIElement> uiElement = std::make_shared<UIElement>();
+
+	// L16: TODO 1: Implement CreateUIElement function that instantiates a new UIElement according to the UIElementType and add it to the list of UIElements
+	switch(type) {
+	case UIElementType::BUTTON:
+		uiElement = std::make_shared<UIButton>(id, bounds, text);
+		break;
+	case UIElementType::TOGGLE:
+		break;
+	case UIElementType::CHECKBOX:
+		break;
+	case UIElementType::SLIDER:
+		break;
+	case UIElementType::SLIDERBAR:
+		break;
+	case UIElementType::COMBOBOX:
+		break;
+	case UIElementType::DROPDOWNBOX:
+		break;
+	case UIElementType::INPUTBOX:
+		break;
+	case UIElementType::VALUEBOX:
+		break;
+	case UIElementType::SPINNER:
+		break;
+	}
+	//set the callback to the observer
+	uiElement->SetObserver(observer);
+	//Add UiElemento to the list
+	UIElementsList.push_back(uiElement);
+	return uiElement;
+}
+
+bool UIManager::Update(float dt)
+{	
+	for (const auto& control : UIElementsList)
+	{
+		control->Update(dt);
+	}
+
+	return true;
+}
+
+bool UIManager::CleanUp()
+{
+	for (const auto& uiElement : UIElementsList)
+	{
+		uiElement->CleanUp();
+	}
+
+	return true;
+}
+
+
